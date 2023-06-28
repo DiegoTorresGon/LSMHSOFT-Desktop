0633f15b29d74f7c8f33c6e6bd6ca7119864cc3e
DiegoTorresGon <a220206322@unison.mx>
2023-06-28 14:12:12 -0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace LSMHSOFT___Desktop
+{
+    partial class LSMHSOFT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ModelFileButton = new System.Windows.Forms.Button();
+            this.OpenModelFile = new System.Windows.Forms.OpenFileDialog();
+            this.DataOutput = new System.Windows.Forms.TextBox();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ModelFileButton
+            // 
+            this.ModelFileButton.Location = new System.Drawing.Point(54, 111);
+            this.ModelFileButton.Name = "ModelFileButton";
+            this.ModelFileButton.Size = new System.Drawing.Size(75, 23);
+            this.ModelFileButton.TabIndex = 0;
+            this.ModelFileButton.Text = "select";
+            this.ModelFileButton.UseVisualStyleBackColor = true;
+            this.ModelFileButton.Click += new System.EventHandler(this.ModelFileButton_Click);
+            // 
+            // OpenModelFile
+            // 
+            this.OpenModelFile.FileName = "ModelFile";
+            this.OpenModelFile.Filter = "GFC Files (*.gfc)|*.gfc|All files (*.*)|*.*";
+            this.OpenModelFile.Title = "Open model file";
+            // 
+            // DataOutput
+            // 
+            this.DataOutput.Location = new System.Drawing.Point(685, 86);
+            this.DataOutput.Multiline = true;
+            this.DataOutput.Name = "DataOutput";
+            this.DataOutput.Size = new System.Drawing.Size(297, 318);
+            this.DataOutput.TabIndex = 0;
+            this.DataOutput.Text = "This is a text";
+            this.DataOutput.TextChanged += new System.EventHandler(this.DataOutput_TextChanged);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.TitleLabel.Location = new System.Drawing.Point(279, 20);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(286, 31);
+            this.TitleLabel.TabIndex = 1;
+            this.TitleLabel.Text = "LSMHSOFT - Desktop";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(54, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(35, 379);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Optional arguments:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(54, 430);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 488);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(79, 525);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(79, 575);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(362, 488);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(362, 525);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // LSMHSOFT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1031, 632);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.DataOutput);
+            this.Controls.Add(this.ModelFileButton);
+            this.Name = "LSMHSOFT";
+            this.Text = "LSMHSOFT - Desktop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ModelFileButton;
+        private System.Windows.Forms.OpenFileDialog OpenModelFile;
+        private System.Windows.Forms.TextBox DataOutput;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LSMHSOFT___Desktop
+{
+    public partial class LSMHSOFT : Form
+    {
+        public LSMHSOFT()
+        {
+            InitializeComponent();
+        }
+
+        private void ModelFileButton_Click(object sender, EventArgs e)
+        {
+            if (OpenModelFile.ShowDialog() == DialogResult.OK)
+            {
+                DataOutput.Text = OpenModelFile.FileName;
+            }
+        }
+
+        private void DataOutput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
